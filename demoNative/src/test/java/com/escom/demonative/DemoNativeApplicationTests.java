8ef6a6cc99e444e8d8640f0ad024d5ce66e9b87d
David Perez Antonio <david.perez.antonio51@gmail.com>
2023-01-26 15:02:15 -0600

    creacion de proyecto

---
+++ demoNative/src/test/java/com/escom/demonative/DemoNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escom.demonative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoNativeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
